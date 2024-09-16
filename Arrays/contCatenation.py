65c75a195e64c9fe0618627a42436d46063a0e4f
kunalg12 <kunalgursal2923@gmail.com>
2024-09-16 21:59:13 +0530

    cont contcatenation

---
+++ Arrays/contCatenation.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(2):  # Repeat twice
+            for n in nums:  # Loop through each element in nums
+                ans.append(n)
+        return ans
+
+
+solution = Solution()
+nums = [1, 2, 3]
+result = solution.getConcatenation(nums)
+print(result)  # Output: [1, 2, 3, 1, 2, 3]
