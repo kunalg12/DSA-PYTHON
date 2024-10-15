642b60401a622564d3e712855c50c78a2384175a
kunalg12 <kunalgursal2923@gmail.com>
2024-10-15 22:53:40 +0530

    updated group anagram

--- Arrays/replGreatestElemToRight.py
+++ Arrays/replGreatestElemToRight.py
@@ -4,11 +4,12 @@ from typing import List
 class Solution:
     def replaceElement(self, arr: List[int]) -> List[int]:
         maxRight = -1
-        for i in range(len(arr)-1, -1, -1):
+        for i in range(len(arr) - 1, -1, -1):
             newMax = max(maxRight, arr[i])
             arr[i] = maxRight
             maxRight = newMax
         return arr
+
 
 solution = Solution()
 arr = [17, 18, 5, 4, 6, 1]
