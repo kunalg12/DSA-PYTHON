062b46ad53901d5ed6f340565a0a49849771de88
kunalg12 <kunalgursal2923@gmail.com>
2024-11-02 17:40:04 +0530

    POTD Circular Sentence

---
+++ LeetCode_daily/Circular_Sentence.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def Circular_Sentence(self, sentence: str) -> bool:
+        # split the sentence into words
+        words = sentence.split()
+        # Number of word in sentence
+        n = len(words)
+
+        # Check the circular conditions
+        for i in range(n):
+            # last char of first word
+            first_char = words[i][-1]
+            # next char of current word
+            next_char = words[(i + 1) % n][0]
+
+            # comparing both char if they don't match its not circular
+            if first_char != next_char:
+                return False
+        return True
+
+
+solution = Solution()
+sentence = "leetcode exercises sound delightful"
+res = solution.Circular_Sentence(sentence)
+print(res)
