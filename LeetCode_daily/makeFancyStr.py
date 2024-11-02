5982167b18435bc9e756a21f257595d601fe80f7
kunalg12 <kunalgursal2923@gmail.com>
2024-11-02 15:22:25 +0530

    leetcode potd makeFancyString

---
+++ LeetCode_daily/makeFancyStr.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeFancyString(self, s:str) -> str :
+        result = []
+
+        for char in s:
+            if len(result) < 2 or not (result[-1] == result[-2] == char):
+                result.append(char)
+        return "".join(result)
+
+
+soution = Solution()
+res = soution.makeFancyString("Leeetcode")
+print(res)
