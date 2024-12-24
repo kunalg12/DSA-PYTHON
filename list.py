62a2931f5c031319f88f1809c8b277ec990b2cb2
kunalg12 <kunalgursal2923@gmail.com>
2024-12-24 23:56:29 +0530

    update

---
+++ list.py
@@ -0,0 +1,16 @@
+List = ["Geeks", "For", "Geeks"]
+print("\nList containing multiple values: ")
+print(List)
+
+List2 = [["Geeks", "For"], ["Geeks"]]
+print("\nMulti-Dimensional List: ")
+print(List2)
+
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+
+print("Accessing element using negative elements")
+
+print(List[-1])
+print(List[-2])
